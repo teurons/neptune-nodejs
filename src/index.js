a8f703203eec64786796720f087927856140e876
Rajiv Seelam <rajiv@betalectic.com>
2022-06-16 18:51:23 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,31 +1,70 @@
+const axios = require("axios");
+
 const DEFAULT_API_BASE = "https://api.teurons.com/neptune";
 const EDGE_API_BASE = "https://edge.teurons.com/neptune";
+
+const defaultAxios = axios.create();
+const edgeAxios = axios.create();
+
+defaultAxios.defaults.baseURL = DEFAULT_API_BASE;
+
+defaultAxios.defaults.headers.common = {
+  "Content-Type": "application/json",
+};
+
+edgeAxios.defaults.baseURL = EDGE_API_BASE;
+
+edgeAxios.defaults.headers.common = {
+  "Content-Type": "application/json",
+};
 
 const init = (env, token) => {
   process.env.NEPTUNE_ENV = env;
   process.env.NEPTUNE_TOKEN = token;
 };
 
-const fetchEnvironments = () => {};
+const fetchEnvironments = () => {
+  return new Promise((resolve, reject) => {
+    let uri = `/environments`;
 
-// $curl = new Curl();
-// $curl->setHeader('Accept', 'application/json');
-// $curl->setHeader('Authorization', 'Bearer '.config('neptune.token'));
-// $curl->get(self::DEFAULT_API_BASE."/environments");
+    defaultAxios
+      .get(uri, {
+        headers: {
+          Authorization: `Bearer ${process.env.NEPTUNE_TOKEN}`,
+        },
+      })
+      .then((response) => {
+        resolve(response.data);
+      })
+      .catch((errors) => {
+        reject(errors);
+      });
+  });
+};
 
-// if ($curl->error) {
-//     echo 'Error: ' . $curl->errorCode . ': ' . $curl->errorMessage . "\n";
-// } else {
-//     echo 'Response:' . "\n";
-//     var_dump($curl->response);
-// }
+const fire = (eventType, data, payload) => {
+  let finalPayload = {
+    event_type: eventType,
+    environment: process.env.NEPTUNE_ENV,
+    api_token: process.env.NEPTUNE_TOKEN,
+    version: "1",
+    data: data,
+  };
 
-const fire = () => {
-  console.log(
-    "Say something cute",
-    process.env.NEPTUNE_ENV,
-    process.env.NEPTUNE_TOKEN
-  );
+  finalPayload = { ...payload, ...finalPayload };
+
+  return new Promise((resolve, reject) => {
+    let uri = `/events/ingest`;
+
+    edgeAxios
+      .post(uri, finalPayload)
+      .then((response) => {
+        resolve(response);
+      })
+      .catch((errors) => {
+        reject(errors);
+      });
+  });
 };
 
 module.exports = { fire, init, fetchEnvironments };
